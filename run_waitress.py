722d6ed48538c34019ec7bd8cf265eeba769bb4d
Solomon <solomonaz2022@gmail.com>
2025-07-10 21:20:12 +0300

    live deployement for production

--- run_waitress.py
+++ run_waitress.py
@@ -1,4 +1,21 @@
 from waitress import serve
 from onlinexam.wsgi import application
+import socket
 
-serve(application, host='0.0.0.0', port=8000)
+# Get local IP address
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
+if __name__ == '__main__':
+    print(f"\nServer running on:\n")
+    print(f"Local: http://localhost:8000")
+    print(f"LAN: http://{local_ip}:8000\n")
+    
+    serve(
+        application,
+        host='0.0.0.0', 
+        port=8000,
+        # threads=4,  
+        url_scheme='http',
+        # channel_timeout=120 
+    )
